9d2da278db28b27fa77118c46dcc72be0973c573
Wei246 <135291290+Wei246@users.noreply.github.com>
2023-06-14 21:44:00 +0800

    Update dish.js

--- src/api/dish.js
+++ src/api/dish.js
@@ -1,4 +1,47 @@
+import {caxios} from "@/js/request";
+// 查询列表接口
+const getDishPage = (params) => {
+    return caxios({
+        url: '/dish/page',
+        method: 'get',
+        params
+    })
+}
 
+// 删除接口
+const deleteDish = (ids) => {
+    return caxios({
+        url: '/dish',
+        method: 'delete',
+        params: {ids}
+    })
+}
+
+// 修改接口
+const editDish = (params) => {
+    return caxios({
+        url: '/dish',
+        method: 'put',
+        data: {...params}
+    })
+}
+
+// 新增接口
+const addDish = (params) => {
+    return caxios({
+        url: '/dish',
+        method: 'post',
+        data: {...params}
+    })
+}
+
+// 查询详情
+const queryDishById = (id) => {
+    return caxios({
+        url: `/dish/${id}`,
+        method: 'get'
+    })
+}
 
 // 获取菜品分类列表
 const getCategoryList = (params) => {
